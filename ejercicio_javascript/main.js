5488d1511765b660c04f31e2ae6b6159f0bf7a29
RSanchez19 <00191018@uca.edu.sv>
2019-10-06 20:56:27 -0600

    add ejercicio javascript

---
+++ ejercicio_javascript/main.js
@@ -0,0 +1,22 @@
+let counter_btn =  document.querySelector("#counter-btn")
+let counter_space =  document.querySelector("#counter-space")
+
+let add = (function () {
+    let counter = 0;
+    
+    return function () {
+        counter += 1; 
+        counter_space.innerHTML = counter;
+        return counter
+    }
+
+})();
+
+counter_btn.addEventListener("click", ()=>{
+        add();
+})
+
+let hardcoreReset = ()=>{
+    counter = 0;
+    counter_space.innerHTML = counter;
+}
